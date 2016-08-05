9c6937d2b61c85f88d949ad90be475ec17c6f4b8
brahian609 <brahian609@hotmail.com>
2016-08-05 11:13:47 -0500

    add component TodoList

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { TodoInput } from "./todo-input";
+import { TodoList } from "./todo-list";
 
 @Component({
     selector: 'my-app',
-    directives: [TodoInput],
-    template: `
-            <h1>Angular 2</h1>
-            <div><todo-input></todo-input></div>
-        `
+    directives: [TodoInput, TodoList],
+    template: `<h1>Angular 2</h1>
+    <div>
+    <todo-input></todo-input>
+    <todo-list></todo-list>
+    </div>`
 })
 
 export class AppComponent { }
