addae85ea3c6ce40ca663fb7ade0e2cbff47e668
brahian609 <brahian609@hotmail.com>
2016-08-05 09:46:42 -0500

    add event click to input

--- app/todo-input.ts
+++ app/todo-input.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'todo-input',
-    template: `<div>Todo input</div>`
+    template: `<div>
+    <input type="text" #myInput>
+    <button (click)="onClick(myInput.value)">Click me</button>
+    </div>`
 })
 
-export class TodoInput { }
+export class TodoInput {
+    onClick(value) {
+        console.log(value);
+    }
+}
